92fdd30235ad529b8631f11dfc226002abef8d94
holaialonso <irenealonso90@hotmail.com>
2024-02-04 15:03:12 +0100

    First Commit

---
+++ app/src/main/java/com/example/compras/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.compras
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import com.example.compras.databinding.ActivityMainBinding
+import androidx.appcompat.widget.Toolbar
+
+
+class MainActivity : AppCompatActivity() {
+
+    //Fichero -> me traigo el layout (activity_main.xml)
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //Parte gráfica + lógica del activity
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Menú
+        var toolbar : Toolbar = findViewById(R.id.toolbar) //busco la toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.title="title"
+
+
+    }
+
+    //MENÚ
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main_activity, menu)
+        return true
+    }
+
+}
